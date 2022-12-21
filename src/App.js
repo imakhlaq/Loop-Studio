458d71dd26e12933739f6e0fe977491750c75516
imakhlaq <imakhlaqXD@gmail.com>
2022-12-21 15:00:16 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Features />
+      <Footer />
     </div>
   );
 }
