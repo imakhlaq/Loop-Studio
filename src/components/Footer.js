4b01b0bc0b4ac69e02cdd2a46edc288745cc1250
imakhlaq <imakhlaqXD@gmail.com>
2022-12-21 15:09:16 +0530

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="bg-black">
       <div className="container  mx-auto flex max-w-6xl flex-col justify-between gap-6 py-2 md:flex-row">
         <div className="mt-4 flex flex-col items-center justify-center">
-          <img src={logo} alt="" className="max-w-xs" />
+          <img src={logo} alt="" className="max-w-xs md:self-start" />
           <div className="mt-4 flex flex-col gap-4 text-white md:flex-row">
             <a href="da">About</a>
             <a href="dada">Career</a>
@@ -19,13 +19,13 @@ const Footer = () => {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center gap-6">
-          <div className="flex items-center justify-center gap-6">
+          <div className="flex items-center justify-center gap-6 md:self-start">
             <img src={twitter} alt="" className="max-w-xs" />
             <img src={insta} alt="" className="max-w-xs" />
             <img src={fb} alt="" className="max-w-xs" />
           </div>
           <p className="font-alata text-gray-400">
-            &copy Loopstudio. All rights reserved
+            &copy; Loopstudio. All rights reserved
           </p>
         </div>
       </div>
